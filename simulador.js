f81db8c347878f489a6f483f85ef2b9073428a53
Lourdes Debiaggi <ldebiaggi@hotmail.com>
2023-02-20 14:20:17 -0300

    pre-etrega3 - Optimización de proyecto Javascript

--- simulador.js
+++ simulador.js
@@ -1,85 +1,89 @@
-
-// EL CONTENIDO DEL HTML ESTÁ EN CONSTRUCCIÓN, hasta que aprendamos a vincular js a objetos.
-
 // La idea del proyecto está inspirado en mi proyecto de Diseño WEB sobre HAUS-Productores de Yerba Mate. El simulador permitirá calcular la producción estimada de Yerba Mate en función de algunas variables (espacio en hectáreas, tiempo, humedad, etc), la aplicación está destinada a usuarios que quieran incursionar en el negocio, o simplemente que sean curiosos.
 
-// Variables iniciales
-let hectareas = parseInt(prompt ("¿Cuántas hectáreas querés cosechar?" ));
+//Declaración de variables para los elementos DOM utilizados en el script.
+const form = document.querySelector("form");
+const hectareasInput = document.querySelector("#hectareas");
+const hectareasOutput = document.querySelector("#hectareasOutput");
+const densidadSelect = document.querySelector("#densidadPlantacion");
+const humedadSelect = document.querySelector("#humedad");
+const tiempoInput = document.querySelector("#tiempo");
+const tiempoOutput = document.querySelector("#tiempoOutput");
+const resultadoElement = document.querySelector("#resultado");
 
-// Densidad (Aplicación OBJETO + Búsqueda)
-let densidades = {
-baja: 1100,
-alta: 2200,
-super: 3200
-};
+//Eventos para los campos de entrada HECTÁREAS y TIEMPO.
+hectareasInput.addEventListener("input", function() {
+  hectareasOutput.textContent = hectareasInput.value;
+});
 
-let densidad = prompt("Elige la densidad de plantación: Baja, Alta o Super");
+tiempoInput.addEventListener("input", function() {
+  tiempoOutput.textContent = tiempoInput.value;
+});
 
-while (!densidades[densidad.toLowerCase()]) {
-densidad = prompt("Ingresaste un dato inválido como densidad, indica si es: Baja, Alta o Super");
+//Función para detectar si los campos DENSIDAD y HUMEDAD han sido seleccionados correctamente, de no ser así, se implementará a futuro un modal con la instrucción para que el usuario pueda corregir el dato ingresado, actualmente como no sabemos usar librerías, se utiliza DOM.
+function validateForm() {
+  let isValid = true;
+  if (densidadSelect.value === "Seleccionar") {
+    resultadoElement.textContent = "Por favor, seleccione una densidad de plantación válida";
+    isValid = false;
+  } 
+  if (humedadSelect.value === "Seleccionar") {
+    resultadoElement.textContent = "Por favor, seleccione una humedad válida";
+    isValid = false;
+  }
+  if (isValid) {
+    resultadoElement.textContent = "";
+  }
+  return isValid;
 }
 
-densidad = densidad.toLowerCase();
+//Evento para cuando se envía el formulario, evita que el mismo se envíe predeterminado y permite que se avance siempre que los campos posean valores admitidos.
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  if (!validateForm()) {
+    return;
+  };
 
-function buscarValorDensidad(densidad) {
-    for (let key in densidades) {
-        if (key === densidad) {
-        return densidades[key];
-        }
-     } 
-    return null;
-    }
-    
-    let valorDensidad = buscarValorDensidad(densidad);
-    if (valorDensidad) {
-    alert("La densidad '" + densidad + "' equivale a: " + valorDensidad + "plantines por hectárea.");
+  const hectareas = parseInt(hectareasInput.value);
+  const densidades = {
+    baja: 1100,
+    alta: 2200,
+    super: 3200
+  };
+
+  const densidad = densidadSelect.value.toLowerCase();
+  const valorDensidad = densidades[densidad];
+  const humedades = [0.4, 0.8, 1.25]; 
+  const humedad = humedadSelect.value.toLowerCase();
+  const humedadIndex = [
+    "muybaja",
+    "baja",
+    "alta"
+  ].indexOf(humedad);
+  
+  const tiempo = parseInt(tiempoInput.value);
+  if (tiempo < 5) {
+    if (tiempo === 1) {
+      resultadoElement.textContent = "Tus plantitas aún son bebés, el mínimo es de 5 años para que crezcan!";
+    } else if (tiempo === 2 || tiempo === 3) {
+      resultadoElement.textContent = "El mínimo son 5 años para que tus plantitas crezcan...";
     } else {
-    alert("La densidad ingresada no se encuentra en la lista");
+      resultadoElement.textContent = "Te falta un año y ya podés cosechar!";
     }
+    return;
+  }
 
-// Humedad (Aplicación Arrays)
-const humedades = [0.4, 0.8, 1.25];
-const humedadesNombres = ["muy baja", "baja", "alta"];
+  //Cálculo Resultado
+  const resultado = hectareas * valorDensidad * humedades[humedadIndex] * tiempo;
 
-let humedad = prompt("Elige la humedad del territorio: Muy baja, Baja, Alta");
+  //Almacenamiento de datos con LocalStorage y JSON.
+  const resultadoJson = JSON.stringify({
+    hectareas,
+    densidad,
+    humedad,
+    tiempo,
+    resultado
+  });
 
-let humedadIndex = humedadesNombres.indexOf(humedad.toLowerCase());
-
-while (humedadIndex === -1) {
-humedad = prompt("Ingresaste un dato inválido. Selecciona la humedad del territorio entre: Muy baja, Baja, Alta");
-humedadIndex = humedadesNombres.indexOf(humedad.toLowerCase());
-}
-
-humedad = humedad.toLowerCase();
-
-//Años cosecha
-let aniosCosecha = parseInt(prompt("¿Por cuantos años dejarás crecer las plantas?"));
-while (aniosCosecha < 5 ) {
-switch (aniosCosecha){
-    case 1:
-        alert("Tus plantitas están aún en modo bebé, mínimo necesitás 5 años para que crezcan!");
-        break;
-    case 2:
-        alert("En mínimo son 5 años para que las plantas alcancen su madurez");
-        break;
-    case 3: 
-        alert("En mínimo son 5 años para que las plantas alcancen su madurez");
-        break;
-    case 4: 
-        alert("Te falta un año y ya podés cosechar y calcular tu producción!");
-        break;
-}
-
-aniosCosecha = parseInt(prompt("¿Por cuántos años dejarás crecer las plantas?"));
-}
-
-//Cálculo final
-let produccionPorHectareaAnual = valorDensidad * humedades[humedadIndex];
-
-function calcularProduccion(produccionPorHectareaAnual, hectareas, aniosCosecha) {
-let produccionTotal = produccionPorHectareaAnual * hectareas * aniosCosecha;
-alert("La producción total de yerba mate será de " + produccionTotal + " kg");
-}
-
-calcularProduccion(produccionPorHectareaAnual, hectareas , aniosCosecha);
-
+  localStorage.setItem("resultado", resultadoJson);
+  resultadoElement.textContent = `La producción total de Yerba Mate será de: ${resultado} Kg.`;
+});
